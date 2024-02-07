29d380161be97d795ac1fed20a65cd0a9c0cfb01
Pragyan Das <36222744+PragyanD@users.noreply.github.com>
2024-02-07 15:59:47 -0600

    ownership continued

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -4,10 +4,16 @@ fn main() {
     s.push_str(", world!"); // push_str() appends a literal to a String
     
     println!("{s}"); // this will print `hello, world!`
+    
+   //following code will raise an error to prevent double free after scope
+    // let s1 = String::from("hello");
+    // let s2 = s1;
+    // println!("{s1}, world!");
+    
     let s1 = String::from("hello");
-    let s2 = s1;
-
-    println!("{s1}, world!");
+    let s2 = s1.clone();
+    
+    println!("s1 = {s1}, s2 = {s2}");
 }
 //need to create a string object from literal to facilitate mutability
 //rust calls drop(s) when string goes out of scope
