2c5cd6d26bbaaf0d0d21ad23171de14778024b51
Pragyan Das <36222744+PragyanD@users.noreply.github.com>
2024-02-14 14:32:01 -0600

    Tuple returns

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,19 +1,32 @@
 fn main() {
-    let mut s = String::from("hello");
-
-    s.push_str(", world!"); // push_str() appends a literal to a String
-    
-    println!("{s}"); // this will print `hello, world!`
-    
-   //following code will raise an error to prevent double free after scope
-    // let s1 = String::from("hello");
-    // let s2 = s1;
-    // println!("{s1}, world!");
-    
     let s1 = String::from("hello");
-    let s2 = s1.clone();
-    
-    println!("s1 = {s1}, s2 = {s2}");
+
+    let (s2, len) = calculate_length(s1);
+
+    println!("The length of '{s2}' is {len}.");
 }
+
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len(); // len() returns the length of a String
+
+    (s, length)
+}
+// fn main() {
+//     let mut s = String::from("hello");
+
+//     s.push_str(", world!"); // push_str() appends a literal to a String
+    
+//     println!("{s}"); // this will print `hello, world!`
+    
+//    //following code will raise an error to prevent double free after scope
+//     // let s1 = String::from("hello");
+//     // let s2 = s1;
+//     // println!("{s1}, world!");
+    
+//     let s1 = String::from("hello");
+//     let s2 = s1.clone();
+    
+//     println!("s1 = {s1}, s2 = {s2}");
+// }
 //need to create a string object from literal to facilitate mutability
 //rust calls drop(s) when string goes out of scope
